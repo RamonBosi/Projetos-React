ce06781edb5e2b77dd99977fa577611eceac4956
Ramon Bosi <ramombosi2015@gmail.com>
2022-04-13 16:48:42 -0300

    Update GlobalStyles.js

--- projetos/src/GlobalStyles.js
+++ projetos/src/GlobalStyles.js
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+
+    *{
+        padding:0;
+        margin: 0;
+        box-sizing: border-box
+    }
+
     body{
         background-color: #3b3b3b;
     }
