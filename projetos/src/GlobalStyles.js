b64fe885fd8fce449879793fddc3a8d836f5fa77
Ramon Bosi <ramombosi2015@gmail.com>
2022-04-13 16:40:53 -0300

    enabling export

--- projetos/src/GlobalStyles.js
+++ projetos/src/GlobalStyles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+export const GlobalStyles = createGlobalStyle`
     body{
         background-color: #3b3b3b;
     }
