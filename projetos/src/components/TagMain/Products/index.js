30a726a1504edd28581da593bc07ce0610b00515
Ramon Bosi <ramombosi2015@gmail.com>
2022-04-24 19:07:19 -0300

    Product algorithm update

--- projetos/src/components/TagMain/Products/index.js
+++ projetos/src/components/TagMain/Products/index.js
@@ -1,29 +1,20 @@
+import { useState } from 'react'
 import { Products } from './styles'
 import Product from './Product'
 
 export default function CreateProducts(){
+
+    const [p, setProducts] = useState([])
+
+    const objProducts = () =>{
+        for(let n = 1; n <= 4; n++){
+            setProducts(...p, n)
+        }
+    }
+
     return(
         <Products>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
+            <button onClick = {() => console.log(p)}>Mostrar</button>
         </Products>
     )
 }
