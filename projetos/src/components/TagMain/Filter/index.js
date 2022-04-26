e1b2cde1f247642bc541cb5071cca9513a93c63b
Ramon Bosi <ramombosi2015@gmail.com>
2022-04-26 15:13:41 -0300

    Filter algorithm update

--- projetos/src/components/TagMain/Filter/index.js
+++ projetos/src/components/TagMain/Filter/index.js
@@ -1,12 +1,12 @@
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
 import { FilterByValue } from './styles'
 
 export default function CreateFilter(){
 
-    const inputMin = useRef()
-    const inputMax = useRef()
-
-    const [filteredProducts, setFilteredProducts] = useState(null)
+    const { valueMin, valueMax } = {
+        valueMin: useRef(),
+        valueMax: useRef()
+    }
 
     const showProducts = () =>{
 
@@ -20,15 +20,18 @@ export default function CreateFilter(){
 
     const filter = (min, max) =>{
 
-        const filterProducts = (products) => {
-                
-            const filteredProducts2 = products.filter((product) => {
+        if(min <= max || max === 0){
+
+            showProducts()
+            const jsonProducts = JSON.parse(localStorage.getItem('standardProducts'))
+
+            jsonProducts.map((product) => {
 
                 const idProduct = String(product.id)
                 const valueProduct = Number(product.value)
-
+                
                 if(min !== 0 && max !== 0){
-
+                        
                     if(valueProduct >= min && valueProduct <= max){
                         return product
                     }else{
@@ -52,24 +55,8 @@ export default function CreateFilter(){
                     }
 
                 }
-
             })
-            setFilteredProducts(filteredProducts2)
 
-        }
-
-        if(min <= max || max === 0){
-
-            if(filteredProducts === null){
-
-                const jsonProducts = JSON.parse(localStorage.getItem('standardProducts'))
-                showProducts()
-                filterProducts(jsonProducts)
-
-            }else{
-                filterProducts(filteredProducts)
-            }
-    
         }else{
             alert('Valor mínimo é maior que o valor máximo. Mude o valor')
         }
@@ -79,11 +66,10 @@ export default function CreateFilter(){
         <FilterByValue>
             <h2>Filtrar por valor</h2>
             <div>
-                <input ref = {inputMin} type = 'number' placeholder='Mínimo'/>
-                <input ref = {inputMax} type = 'number' placeholder='Máximo'/>
+                <input ref = {valueMin} type = 'number' placeholder='Mínimo'/>
+                <input ref = {valueMax} type = 'number' placeholder='Máximo'/>
                 <button onClick = {() => filter(
-                    Number(inputMin.current.value), Number(inputMax.current.value))}>Filtrar</button>
-                    <button onClick = {() => showProducts()}>Resetar</button>
+                    Number(valueMin.current.value), Number(valueMax.current.value))}>Filtrar</button>
             </div>
         </FilterByValue>
     )
