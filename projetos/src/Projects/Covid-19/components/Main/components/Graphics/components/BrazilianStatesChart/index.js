2beefc85ee1ccb28b355bfd590e28db3468d4318
Ramon Bosi <ramombosi2015@gmail.com>
2022-05-13 11:28:35 -0300

    Project "Covid-19"
    Constructing the graph of "Covid-19" by Brazilian State

---
+++ projetos/src/Projects/Covid-19/components/Main/components/Graphics/components/BrazilianStatesChart/index.js
@@ -0,0 +1,19 @@
+import { Content } from './styles'
+// import Graphic from './Graphic'
+
+export default function BrasilianStatesChart(){
+    return(
+        <Content>
+            <header><h2>Gráfico por Estado brasileiros</h2></header>
+            <section>
+                <p>Informações referentes a evolução da covid-19 em cada Estado brasileiro</p>
+                <select>
+                    <option>São Paulo</option>
+                    <option>Rio de Janeiro</option>
+                    <option>Espírito Santo</option>
+                </select>
+            </section>
+            {/* <Graphic/> */}
+        </Content>
+    )
+}
---
+++ projetos/src/Projects/Covid-19/components/Main/components/Graphics/components/BrazilianStatesChart/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Content = styled.section`
+
+    > header{
+        margin: 0 0 10px 0;
+    }
+
+    > section{
+
+        > select{
+            width: 30%;
+            margin: 10px 0 0 0;
+            font-size: 2rem;
+            color: var(--default-white);
+            background-color: var(--primary-elements-background-color);
+            padding: 15px;
+            border-radius: 10px;
+        }
+    }
+`
