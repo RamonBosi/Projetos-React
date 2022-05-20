650f6837d661b5b89e89cff15693b2cceb6a6391
Ramon Bosi <ramombosi2015@gmail.com>
2022-05-20 12:08:23 -0300

    Project "Covid-19"
    I organized the "Graphic" file from the "Brazil Graphic" folder

--- projetos/src/Projects/Covid-19/components/Main/components/Graphics/components/BrazilGraphic/Graphic/index.js
+++ projetos/src/Projects/Covid-19/components/Main/components/Graphics/components/BrazilGraphic/Graphic/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from 'react'
+import { useEffect, useState, useContext, useMemo } from 'react'
 import { ContextAPI } from '../../../../../../../Contexts/API'
 import { Content } from "./styles"
 import { Chart } from 'react-google-charts'
@@ -6,51 +6,14 @@ import { Chart } from 'react-google-charts'
 export default function Graphic(){
 
     const { API } = useContext(ContextAPI)
+
     const [data, setData] = useState(null)
     const [chartTitle, setChartTitle] = useState(null)
 
     useEffect(()=>{ loadChart('cases', 'Casos') },[])
- 
-    const { stylesChart, options } = {
-        stylesChart: {
-            width: '99%',
-            height: '90%',
-            margin: 'auto'
-        },
-        chartColors(){
-            const { cases, suspects, deaths } = {
-                cases: ['rgb(86, 150, 214)', 'rgb(4, 53, 189)'],
-                suspects: ['rgb(232, 227, 74)','rgb(252, 240, 3)'],
-                deaths: ['rgb(222, 137, 131)','rgb(250, 21, 5)']
-            }
-    
-            if(chartTitle === 'Casos'){
-                return cases
-            }else if(chartTitle === 'Suspeitos'){
-                return suspects
-            }else{
-                return deaths
-            }
-        },
-        options: {
-            region: 'BR',
-            resolution: 'provinces',
-            backgroundColor: 'rgb(104, 148, 171)',
-            datalessRegionColor:'rgb(195, 208, 214)',
-            tooltip:{
-                textStyle: {
-                    color: 'rgb(0,0,0)',
-                    fontSize: 20
-                }
-            },
-            colorAxis: {
-                colors: this.chartColors()
-            }
-        }
-    }
 
     const loadChart = (situation,title) =>{
-
+        
         setChartTitle(title)
 
         API.get()
@@ -65,6 +28,46 @@ export default function Graphic(){
             ])
         })
         .catch(() => console.log('Algo deu errado!'))
+    }
+
+    const chartColors = useMemo(() =>{
+
+        const { cases, suspects, deaths } = {
+            cases: ['rgb(86, 150, 214)', 'rgb(4, 53, 189)'],
+            suspects: ['rgb(201, 173, 107)','rgb(222, 169, 47)'],
+            deaths: ['rgb(222, 137, 131)','rgb(250, 21, 5)']
+        }
+
+        if(chartTitle === 'Casos'){
+            return cases
+        }else if(chartTitle === 'Suspeitos'){
+            return suspects
+        }else{
+            return deaths
+        }
+    },[chartTitle])
+
+    const { stylesChart, options } = {
+        stylesChart: {
+            width: '99%',
+            height: '90%',
+            margin: 'auto'
+        },
+        options: {
+            region: 'BR',
+            resolution: 'provinces',
+            backgroundColor: 'rgb(104, 148, 171)',
+            datalessRegionColor:'rgb(195, 208, 214)',
+            tooltip:{
+                textStyle: {
+                    color: 'rgb(0,0,0)',
+                    fontSize: 20
+                }
+            },
+            colorAxis: {
+                colors: chartColors
+            }
+        }
     }
 
     return(
